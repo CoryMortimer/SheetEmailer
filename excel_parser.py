4e651ad6acb801c56f176e4effda5c45eb2452aa
Cory Mortimer <cory.m.mortimer@gmail.com>
2016-11-24 19:17:53 -0800

    Add logging and copy over center header

--- excel_parser.py
+++ excel_parser.py
@@ -6,11 +6,15 @@ from sys import exit
 import os
 from Tkinter import Tk
 from tkFileDialog import askdirectory
+import logging.handlers
 from win32com import client
 
 EMAIL_FILE = 'emails.txt'
 SUBJECT_FILE = 'subject.txt'
 BODY_FILE = 'body.txt'
+MAX_LOG_SIZE = 2000000
+LOG_FILENAME = 'sheetEmailer.log'
+BACKUP_LOGS = 5
 
 class SheetEmailer:
     
@@ -24,20 +28,19 @@ class SheetEmailer:
         self._read_subject()
         self._read_body()
 
-
-        print 'Please choose a directory'
+        logger.debug('Please choose a directory')
         Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
         directory = askdirectory()
         
         if directory:
-            print 'Chosen directory: ' + directory
+            logger.debug('Chosen directory: ' + directory)
             self.workbook_directory = directory
         else:
             exit('Please choose a valid directory')
 
     def _read_emails(self):
         email_sources_path = os.path.abspath(EMAIL_FILE)
-        print 'Loading email address from ' + email_sources_path
+        logger.debug('Loading email address from ' + email_sources_path)
         try:
             email_file = open(EMAIL_FILE, 'r')
         except:
@@ -50,44 +53,44 @@ class SheetEmailer:
             self.client_to_email[client_number] = client_email.strip().split(',')
             self.client_to_email[client_number] = [c.strip() for c in self.client_to_email[client_number]]
 
-        print 'Email mapping ' + str(self.client_to_email)
+            logger.debug('Email mapping ' + str(self.client_to_email))
         
     def _read_subject(self):
         subject_sources_path = os.path.abspath(SUBJECT_FILE)
-        print 'Loading subject from ' + subject_sources_path
+        logger.debug('Loading subject from ' + subject_sources_path)
         try:
             with open(subject_sources_path, 'r') as content_file:
                 self.subject = content_file.read()
         except:
             exit(subject_sources_path + ' does not exist')
 
-        print 'Subject: ' + self.subject
+        logger.debug('Subject: ' + self.subject)
         
     def _read_body(self):
         body_sources_path = os.path.abspath(BODY_FILE)
-        print 'Loading body from ' + body_sources_path
+        logger.debug('Loading body from ' + body_sources_path)
         try:
             with open(body_sources_path, 'r') as content_file:
                 self.body = content_file.read()
         except:
             exit(body_sources_path + ' does not exist')
 
-        print 'Subject: ' + self.body
+        logger.debug('Body: ' + self.body)
             
     def create_user_directories_and_files(self):
         for filename in os.listdir(self.workbook_directory):
-            print 'Working on workbook: ' + filename
+            logger.debug('Working on workbook: ' + filename)
             workbook_name = filename.split('.')[0]
             full_workbook_path = os.path.join(self.workbook_directory, filename)
             try:
                 loaded_workbook = load_workbook(filename = full_workbook_path)
             except:
-                print "Unable to load workbook " + filename + ". Will continue to next workbook."
+                logger.debug("Unable to load workbook " + filename + ". Will continue to next workbook.")
                 continue
             
             
             for sheet_name in loaded_workbook.sheetnames:
-                print 'Working on sheet: ' + sheet_name
+                logger.debug('Working on sheet: ' + sheet_name)
                 current_sheet = loaded_workbook[sheet_name]
                 
                 new_workbook, user_number = self._create_workbook_from_sheet(current_sheet)
@@ -104,6 +107,8 @@ class SheetEmailer:
         for row_num, row in enumerate(current_sheet_rows):
             for col_num, cell in enumerate(row):
                 active_sheet.cell(column=col_num + 1, row=row_num + 1).value = cell.value
+
+        active_sheet.oddHeader.center = current_sheet.oddHeader.center
                 
         return new_workbook, user_number
     
@@ -114,7 +119,7 @@ class SheetEmailer:
         
         absolute_path_to_file = os.path.abspath(new_file_path)
     
-        print "Writing " + absolute_path_to_file
+        logger.debug("Writing " + absolute_path_to_file)
         
         self.absolute_file_paths.append(absolute_path_to_file)
     
@@ -124,22 +129,22 @@ class SheetEmailer:
     def _create_directory_if_none_exists(self, user_number):
         absoulte_path_to_sheet = os.path.abspath(user_number)
         if not os.path.exists(user_number):
-            print "Creating directory: " + absoulte_path_to_sheet
+            logger.debug("Creating directory: " + absoulte_path_to_sheet)
             os.makedirs(user_number)
         else:
-            print "Directory already exists: " + absoulte_path_to_sheet
+            logger.debug("Directory already exists: " + absoulte_path_to_sheet)
             
     def convert_all_to_pdf(self):
-        print 'Starting excel'
+        logger.debug('Starting excel')
         xlApp = client.Dispatch("Excel.Application")
         for path in self.absolute_file_paths:
-            print 'Converting ' + path + ' to pdf'
+            logger.debug('Converting ' + path + ' to pdf')
             books = xlApp.Workbooks.Open(path)
             ws = books.Worksheets[0]
             ws.Visible = 1
             pdf_path = os.path.join(os.path.dirname(path), os.path.basename(path).split('.')[0] + '.pdf')
             ws.ExportAsFixedFormat(0, pdf_path)
-            print 'Exported ' + pdf_path
+            logger.debug('Exported ' + pdf_path)
         xlApp.quit()
 
 
@@ -155,7 +160,7 @@ class SheetEmailer:
                 for filename in os.listdir(absoulte_path_to_directory):
                     if filename.endswith(".pdf"):
                         path_to_pdf = os.path.join(absoulte_path_to_directory, filename)
-                        print 'Adding pdf: ' + path_to_pdf
+                        logger.debug('Adding pdf: ' + path_to_pdf)
                         mail.Attachments.Add(Source=path_to_pdf )
                 mail.Save()
             else:
@@ -167,9 +172,26 @@ class SheetEmailer:
         outlook.quit()
 
 if __name__ == "__main__":
-    print 'Starting'
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M',
+                        filename=LOG_FILENAME)
+
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+    logger = logging.getLogger(__name__)
+    handler = logging.handlers.RotatingFileHandler(
+        LOG_FILENAME, maxBytes=MAX_LOG_SIZE, backupCount=BACKUP_LOGS)
+
+    logger.addHandler(handler)
+
+
+    logger.debug('Starting')
     sheet_emailer = SheetEmailer()
     sheet_emailer.create_user_directories_and_files()
     sheet_emailer.convert_all_to_pdf()
     sheet_emailer.save_email_to_draft()
-    print 'Done'
+    logger.debug('Done')
